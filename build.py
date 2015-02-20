5703df65b062f7702a24b8dce196e7f15f3e0b36
Guilherme Mattar Bastos <guilhermembastos@gmail.com>
2015-02-20 13:07:47 -0200

    Page header.

--- build.py
+++ build.py
@@ -1,29 +1,28 @@
-import staticjinja
 import os
 import yaml
-import sys
-from slugify import slugify
-from dateutil.parser import parse
+import staticjinja
+
 
 # We define constants for the deployment.
 cwd = os.getcwd()
-searchpath  = os.path.join(cwd, "templates")
-outputpath  = os.path.join(cwd, "site")
+searchpath = os.path.join(cwd, "templates")
+outputpath = os.path.join(cwd, "site")
 
 
 # We load the data we want to use in the templates.
-PEOPLE = yaml.load(open('data/people.yaml'))
+people = yaml.load(open('data/people.yaml'))
 
 
 def loadAcademyData():
-	return { 'people': PEOPLE,
-					 'resources': None }
+    return {'people': people, 'resources': None}
 
 
 site = staticjinja.make_site(
-	searchpath=searchpath,
-	outpath=outputpath,
-	staticpaths=['static', '../data'],
-	contexts=[(r'.*.html', loadAcademyData),]
-	)
+    searchpath=searchpath,
+    outpath=outputpath,
+    staticpaths=['static', '../data'],
+    contexts=[(r'.*.html', loadAcademyData), ]
+)
+
+
 site.render(use_reloader=True)
